612b8dd2fde2aad2a13b9f057845a12572a989e9
lyl <2326179669@qq.com>
2023-02-09 15:21:13 +0800

    项目初始化-引入vant组件库

--- src/main.js
+++ src/main.js
@@ -4,8 +4,13 @@ import router from './router'
 import store from './store'
 // 加载全局样式
 import '@/styles/index.less'
-Vue.config.productionTip = false
 
+// 引入vant组件
+import Vant from 'vant'
+import 'vant/lib/index.css'
+// 注册使用vant组件库
+Vue.use(Vant)
+Vue.config.productionTip = false
 new Vue({
   router,
   store,
